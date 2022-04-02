2aa674b3682c5bc416cbfbb453616e229cd6c453
Revaleri <101335098+Revaleri@users.noreply.github.com>
2022-04-03 03:03:57 +0700

    Delete xsu.sh

--- xsu.sh
+++
@@ -1,9 +0,0 @@
-#! /data/data/com.termux/files/usr/bin/bash
-su -c '
-xsu_env=$PATH:/data/data/com.termux/files/usr/bin
-xsu_env=$xsu_env:/data/data/com.termux/files/usr/bin/applets
-xsu_env=$xsu_env:/data/data/com.termux/files/home/.termux/bin
-export PATH=$xsu_env; exec su'
-
-# Author: *_*
-# Mail: reload8686@gmail.com
